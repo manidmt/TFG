abc63ca1e267b25ef4524c44b72138246db5b543
manidmt <manidmt5@gmail.com>
2025-04-30 23:05:00 +0200

    Working on Model Indicator

---
+++ financial/momentum/indicators/modelIndicator.py
@@ -0,0 +1,20 @@
+'''
+General Model Indicator Class
+
+@author: Manuel Díaz-Meco (manidmt5@gmail.com)
+'''
+
+from financial.strategies.technical.indicator import TechnicalIndicator
+from financial.data import Variable
+
+
+class ModelIndicator(TechnicalIndicator):
+
+    def __init__(self, model):
+        self.model = model
+
+    
+    def get_data_descriptor(self, input_descriptor):
+        ticker = str(input_descriptor)
+        ticker_model = self.model + '_' + ticker
+        return Variable(ticker_model)
